d555ce76f242850535a91c4a5206b6afe4478d62
awang <awangrhomadon@gmail.com>
2024-03-08 11:01:44 +0700

    condition and assert

--- lib/data/dart/condition/condition.dart
+++ lib/data/dart/condition/condition.dart
@@ -69,3 +69,16 @@ class Condition {
     }
   }
 }
+
+class Assert {
+  ////Assert untuk mengetahui suatu error code didalam suatu kondisi
+  ////default syntaxnya sbg berikut
+  /// assert(condition);
+  // or
+  ///assert(condition, "Your custom message");
+
+  static usingAssert() {
+    var age = 10;
+    assert(age != 10, 'must be 10');
+  }
+}
